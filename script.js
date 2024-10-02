c0a96a4d2f2b0d81a5bddd175c211a92c70b9b5b
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-10-02 20:23:03 +0600

    Add interval using JS

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-const slideshowELement = document.querySelectorAll(".slideshow-element");
+const slideshowElements = document.querySelectorAll(".slideshow-element");
 
 // console.log(slideshowELement);
+
+let countElements = 1;
+
+setInterval(() => {
+  countElements ++;
+  let currentElement = document.querySelector(".current");
+
+  currentElement.classList.remove("current");
+
+  if (countElements > slideshowElements.length) {
+    slideshowElements[0].classList.add("current");
+    countElements = 1;
+  } else{
+    currentElement.nextElementSibling.classList.add("current");
+  }
+
+  
+}, 2000);
+
